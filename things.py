90d86e0d22f06c909eb9b6a61c62e48b3a43ede0
1p22geo <1p22geodecki@gmail.com>
2022-10-14 20:01:25 +0200

    added complex number class

--- things.py
+++ things.py
@@ -2,3 +2,12 @@ class Complex:
     def __init__(self, a, b):
         self.a = a
         self.b = b
+
+    def add(self, x):
+        return Complex(self.a + x.a, self.b + x.b)
+
+    def sub(self, x):
+        return Complex(self.a - x.a, self.b - x.b)
+
+    def mul(self,x):
+        return Complex((self.a*x.a)-(self.b*x.b),(self.a*x.b)+(self.b*x.a))
