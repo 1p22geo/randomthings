c37b44e9c9e9814caee0307db4a69809b94f093e
1p22geo <1p22geodecki@gmail.com>
2022-10-14 21:11:38 +0200

    continuing complex support

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import things
 np.random.seed(19680801)
 
 fig, ax = plt.subplots()
@@ -7,8 +8,8 @@ dots = np.linspace(0.3, 1.2, 10)
 X, Y = np.meshgrid(dots, dots)
 x, y = X.ravel(), Y.ravel()
 
-for dotx in x:
-    for doty in y:
+for x0 in range(len(x)):
+    for y0 in range(len(y)):
         pass
     
 print(y)
--- things.py
+++ things.py
@@ -11,3 +11,15 @@ class Complex:
 
     def mul(self,x):
         return Complex((self.a*x.a)-(self.b*x.b),(self.a*x.b)+(self.b*x.a))
+
+    def __str__(self):
+        if self.b >=0:
+            return "{} + {}i".format(self.a,self.b)
+        else:
+            return "{} - {}i".format(self.a,-self.b)
+
+if __name__ =="__main__":
+    x = Complex(2,3)
+    y = Complex(3,8)
+    print(x.mul(y))
+    print(Complex(-1.34, -1.34))
