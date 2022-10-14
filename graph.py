0b1857270bf06b0f6b9b953601c23068c98ec926
1p22geo <1p22geodecki@gmail.com>
2022-10-14 21:22:14 +0200

    still not working

--- graph.py
+++ graph.py
@@ -1,16 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import things
+from things import Complex as Comp
 np.random.seed(19680801)
 
 fig, ax = plt.subplots()
-dots = np.linspace(0.3, 1.2, 10)
+dots = np.linspace(0, 1, 5)
 X, Y = np.meshgrid(dots, dots)
 x, y = X.ravel(), Y.ravel()
 
+a = Comp(2,3)
+b = Comp(4,-2)
+
 for x0 in range(len(x)):
     for y0 in range(len(y)):
-        pass
+        f = Comp(x[x0], y[y0])
+        q = f.mul(b)
+        r = q.add(a)
+        x[x0] = r.a
+        y[y0] = r.b
     
 print(y)
 print(x)
