90a4bb464bea208581c5ac976ab4828ae6d0a9db
1p22geo <1p22geodecki@gmail.com>
2022-10-14 21:46:17 +0200

    Revert "not working"
    This reverts commit 9cb8fcd746722ee45e6215f2b56a8c20244d3adc.
    
    I said, it's not working

--- graph.py
+++ graph.py
@@ -5,13 +5,10 @@ np.random.seed(19680801)
 
 SAMPLE_SIZE = 10
 
-fig, ax = plt.subplots(2)
+fig, ax = plt.subplots()
 dots = np.linspace(0, 1, SAMPLE_SIZE)
 X, Y = np.meshgrid(dots, dots)
 x, y = X.ravel(), Y.ravel()
-x1, y1 = x, y
-x2, y2 = x, y
-x3, y3 = x, y
 
 a = Comp(2,3)
 b = Comp(4,-2)
@@ -25,22 +22,8 @@ for n in range(SAMPLE_SIZE**2):
     r = q.add(a)
     x[n] = r.a
     y[n] = r.b
-
-a1 = Comp(2,3)
-b1 = Comp(4,-2)
-
-for n in range(SAMPLE_SIZE**2):
-    print(n)
-    f = Comp(x1[n], y1[n])
-
-    """     q = f.mul(b1)
-    r = q.add(a1) """
-
-    x1[n] = r.a
-    y1[n] = r.b
     
 """ print(y)
 print(x) """
-ax[0].scatter(x, y)
-ax[1].scatter(x1, y1)
+ax.scatter(x, y)
 plt.show()
